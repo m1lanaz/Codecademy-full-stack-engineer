0283470589f2b2843e68d4ff50d064813eeab606
Milana Zaborovska <Milana.zaborovska@gmail.com>
2022-10-23 14:50:10 +0100

    create generateTarget func

---
+++ JS/Number Guesser/script.js
@@ -0,0 +1,13 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+};
+
+const compareGuesses = (human, computer, target) => {
+    
+}
