06770df1326c2f1dc496d103c790df631ff17d25
Milana Zaborovska <Milana.zaborovska@gmail.com>
2022-10-23 15:17:30 +0100

    change compareguesses func

--- JS/Number Guesser/script.js
+++ JS/Number Guesser/script.js
@@ -4,21 +4,28 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
-const generateTarget = Math.floor(Math.random() * 10);
-
+ const generateTarget = () => {
+    Math.floor(Math.random() * 10);
+};
 
 const compareGuesses = (human, computer, target) => {
-    if (human === computer) {
-        return true;
-    } else if (human === target) {
-        return true;
-    } else if (computer === target) {
-        return false;
-    } else if ((Math.abs(human - target)) > (Math.abs(computer - target))) {
+    if ((Math.abs(human - target)) > (Math.abs(computer - target))) {
         return false
     } else if ((Math.abs(human - target)) < (Math.abs(computer -target))) {
         return true;
-    }
+    };
+};
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore ++ ;
+    } else if (winner === 'computer') {
+        computerScore ++;
+    };
+};
+
+const advanceRound = () => {
+    currentRoundNumber ++;
 };
 
 
