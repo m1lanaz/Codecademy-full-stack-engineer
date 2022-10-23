e927035b432eb043c7d64cb84720d42f89d03d59
Milana Zaborovska <Milana.zaborovska@gmail.com>
2022-10-23 15:01:19 +0100

    changed generateTarget and added compare guesses

--- JS/Number Guesser/script.js
+++ JS/Number Guesser/script.js
@@ -4,10 +4,21 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
-const generateTarget = () => {
-    return Math.floor(Math.random() * 10);
-};
+const generateTarget = Math.floor(Math.random() * 10);
+
 
 const compareGuesses = (human, computer, target) => {
-    
-}
+    if (human === computer) {
+        return true;
+    } else if (human === target) {
+        return true;
+    } else if (computer === target) {
+        return false;
+    } else if ((Math.abs(human - target)) > (Math.abs(computer - target))) {
+        return false
+    } else if ((Math.abs(human - target)) < (Math.abs(computer -target))) {
+        return true;
+    }
+};
+
+
