221ac0086c3164b048b8f5cc124d1cde64bbe8e5
Milana Zaborovska <Milana.zaborovska@gmail.com>
2022-11-23 18:59:26 +0000

    add setter method for _meal property

--- JS/objects/mealMaker.js
+++ JS/objects/mealMaker.js
@@ -1,4 +1,10 @@
 const menu = {
     _meal : ' ',
     _price : 0,
-  }
+    set meal(mealToCheck) {
+      if (mealToCheck === 'string'){
+        return _meal = mealToCheck;
+      } 
+    }
+  };
+  
