ad27abb21e45522520697a77731ae7ce7634641a
Milana Zaborovska <Milana.zaborovska@gmail.com>
2022-11-23 19:19:43 +0000

    created team object with properties and arrays

---
+++ JS/objects/teamStats.js
@@ -0,0 +1,12 @@
+const team = {
+    _players : [
+      {firstName: 'Milana', lastName: 'Zab', age: 17}, 
+      {firstName: 'Molly', lastName: 'random', age:18},
+      {firstName: 'Nate', lastName: 'Jacobs', age:18}],
+    _games : [
+      {opponent: 'South', teamPoints: 7, opponentPoints: 2},
+      {opponent: 'West', teamPoints: 3, opponentPoints: 5},
+      {opponent: 'East', teamPoints: 6, opponentPoints: 1}
+    ],
+    
+  }
